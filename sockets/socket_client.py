bb4a84d8f746c71ba226783b60b566d4d835cd7d
adkadt <adamkb2004@gmail.com>
2025-10-05 02:37:46 -0400

    adds check for response from server

--- sockets/socket_client.py
+++ sockets/socket_client.py
@@ -1,11 +1,43 @@
 import socket
 import serial
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("192.168.1.9", 9600))
-ser = serial.Serial("/dev/ttyACM0", 9600)
+import time
 
-while True:
-# receive data from the server and decoding to get the string.
-  s.send(ser.readline().decode().strip().encode())
-# close the connection 
-s.close()
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("192.168.1.9", 9600))                # ip address should probably be 192.168.1.7
+ser = serial.Serial("/dev/ttyACM0", 9600)       # baud rate probably should be 115200
+
+try:
+    while True:
+        # receive data from the server and decoding to get the string.
+        msg = ser.readline().decode().strip().encode()
+
+        if msg == '':
+            continue
+
+        s.send(msg)
+        send_time = time.time()
+        attempts = 1
+
+        # check for response
+        response_ack = False
+        while not response_ack:
+            response_msg = s.recv(1024)
+            response_ack = (response_msg == f"ACK: {msg}")
+            
+            # if no response within a second, retry send message
+            if time.time() - send_time >= 1:
+                s.send(msg)
+                send_time = time.time()
+                attempts += 1
+
+            # if no response after 5 attempts give up
+            if attempts > 5:
+                print(f"No response | msg:<{msg}>")
+                break
+
+except KeyboardInterrupt:
+    print("Interrupted by user")
+
+finally:
+    # close the connection 
+    s.close()
