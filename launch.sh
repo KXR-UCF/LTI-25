42496b28f80a3bdf1096d1c4874f3634fa178216
Logan Elkins <loganelkins0101@gmail.com>
2025-11-14 11:56:37 -0500

    Merge launch scripts with main (#9)
    * Added cosmo launch script
    
    * Fixed cosmo script
    
    * Added comments
    
    * Update readme
    
    * Update launch script
    
    * final launch script update
    
    * Updated shutdown logic
    
    * Fixed cleanup function
    
    * Fixed variable names
    
    ---------
    
    Co-authored-by: Richard Phillips <117881439+Wrath-Storm@users.noreply.github.com>

---
+++ launch.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Automates start of cosmo
+# To run:
+# Give script command priviledges: chmod +x launch.sh
+# Run script: ./launch.sh
+
+# Kills all program
+cleanup()
+{
+    echo "Stopping system..."
+    kill -9 $cmd_pid $back_pid $front_pid 2>/dev/null
+    exit 0
+}
+
+# Start command client
+echo "Starting command client..."
+cd cosmo/command_client
+python3 socket_client.py &
+cmd_pid=$!
+
+# Start backend websocket server
+echo "Starting backend..."
+cd ..
+cd ground_station/backend/
+npm start &
+back_pid=$!
+
+# Start frontend interface
+echo "Starting frontend..."
+cd ..
+cd new_frontend/my-app/
+npm run dev &
+front_pid=$!
+
+trap cleanup SIGINT SIGTERM
+echo "Cosmo is running - Press Ctrl+C to stop"
+wait
