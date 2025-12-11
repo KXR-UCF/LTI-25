55ca6d671d4da215ce735824c5abae807785caa0
adkadt <adamkb2004@gmail.com>
2025-12-11 16:24:21 -0500

    changed autostart

--- Wanda/Systemd/startWanda.sh
+++ Wanda/Systemd/startWanda.sh
@@ -8,27 +8,29 @@ echo "Using Production Directory"
 
 source /home/lti/venv/bin/activate
 
-socket_pid=''
 if [ "$is_controller" = "true" ] ; then
+    echo "Controller Pi Configuration"
     # start questdb container
     sudo docker start $questdb_container
     echo "Sent docker start command"
+else
+    echo "Worker Pi Configuration"
+fi
 
-    # check if QuestDB is active
-    echo "waiting for questdb"
-    until curl -s http://localhost:9003/status | grep -q "Healthy"; do
-        # echo "waiting for questdb"
-        sleep 1
-    done
-    echo "Questdb Started"
+# check if QuestDB is active
+echo "waiting for questdb"
+until curl -s http://192.168.1.30:9003/status | grep -q "Healthy"; do
+    sleep 1
+done
+echo "Questdb Active"
 
+
+socket_pid=''
+if [ "$is_controller" = "true" ] ; then
     python3 -u /home/lti/Production/Controls/controllerSocketServer.py >> socket.out 2>&1 &
     socket_pid=$!
     echo "Socket Server Started"
 else
-    echo "is set to not be controller"
-    echo "skipping docker"
-
     python3 -u /home/lti/Production/Controls/workerSocketClient.py >> socket.out 2>&1 &
     socket_pid=$!
     echo "Socket Client Started"
