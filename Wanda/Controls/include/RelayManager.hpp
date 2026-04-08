a3dbc16030a4d24412bbab1263f7acd06775b1aa
adkadt <57259688+adkadt@users.noreply.github.com>
2026-04-07 21:13:43 -0400

    fixed multiple declarations

--- Wanda/Controls/include/RelayManager.hpp
+++ Wanda/Controls/include/RelayManager.hpp
@@ -2,10 +2,12 @@
 
 #include <vector>
 
-// vector of relay pins in order in physical
-std::vector<int> relay_pins = {5, 6, 13, 16, 19, 20, 21, 26};
 
 class RelayManager {
+private:
+    // vector of relay pins in order in physical
+    std::vector<int> relay_pins = {5, 6, 13, 16, 19, 20, 21, 26};
+
 public:
     void setup();
     void set_relay(int relay_id, bool state);
