ff81338e29c832537621bf753c916d9c2c5903f7
adkadt <57259688+adkadt@users.noreply.github.com>
2026-04-07 21:09:53 -0400

    setup basic function declarations

--- Wanda/Controls/include/CommandProcessor.hpp
+++ Wanda/Controls/include/CommandProcessor.hpp
@@ -1,10 +1,29 @@
 #pragma once
 
-#include "RelayManager.hpp"
-#include <cstdint>
+#include <map>
+#include <string>
+#include <functional>
+#include <vector>
+#include <atomic>
 
+#include "RelayManager.hpp"
+
+using OverrideFunc = std::function<void(RelayManager&, std::atomic<bool>&)>;
 
 class CommandProcessor {
+private:
+    std::map<std::string, OverrideFunc> overrides;
+
+    std::atomic<bool> sequence_running{false};
+    std::atomic<bool> abort_signal{false};
+
+    void execute_override(const std::string& name, RelayManager& relay_manager);
+
 public:
     void process(uint32_t state_data);
+
+    void register_override(std::string name, OverrideFunc func);
+
+    void request_abort();
+    void clear_abort();
 };
